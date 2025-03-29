d07e83d55f5acdeb900ca6a9dc5b12fe1c131ffa
Ycitus <7372895+ycycycc123@user.noreply.gitee.com>
2025-03-29 14:30:27 +0800

    v2.0.0 新增像港

--- static/js/print.js
+++ static/js/print.js
@@ -403,6 +403,20 @@ function print_DJ_huanbaobiao() {
     updateClientCount();
 }
 
+function print_XG_huanbaobiao() {
+    document.getElementById('fileTypeMT').innerText = '正在合成，请勿重复点击'
+    fetch('/print_XG_huanbaobiao')
+   .then(response => response.text())
+   .then(data => {
+        document.getElementById('fileTypeMT').innerText = data.split('|')[0]
+        if(data.split('|').length > 1) {
+            setTimeout(printComplete, parseInt(data.split('|')[1])*100)
+        }
+        console.log(data);
+    });
+    updateClientCount();
+}
+
 function print_XGZZ_huanbaobiao() {
     document.getElementById('fileTypeMT').innerText = '正在合成，请勿重复点击'
     fetch('/print_XGZZ_huanbaobiao')
@@ -478,6 +492,15 @@ function craft_DJ_huanbaobiao() {
     document.getElementById('fileTypeMT').innerText = '合成完成';
     const link = document.createElement('a');
     link.href = '/craft_DJ_huanbaobiao';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function craft_XG_huanbaobiao() {
+    document.getElementById('fileTypeMT').innerText = '合成完成';
+    const link = document.createElement('a');
+    link.href = '/craft_XG_huanbaobiao';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
